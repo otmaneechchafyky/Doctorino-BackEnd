1b15577ac0aef22fd176111027adcd55c9840569
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-21 18:57:57 +0100

    Add index action to genres controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,10 @@
 class GenresController < ApplicationController
   def index
+    @genres = []
+    @genres_list = Genre.all
+    @genres_list.each do |genre|
+      @genres << GenreSerializer.new(genre).serializable_hash[:data][:attributes]
+    end
+    render json: @genres
   end
 end
