b2ba1457d859addff9f6e956f24fbd71694e85df
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-21 13:31:21 +0100

    Update vets controller to show specific vet usin id

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -1,19 +1,33 @@
 class VetsController < ApplicationController
+  before_action :set_vet, only: %i[show]
   def index
     @vets = []
     @vets_list = Vet.all
     @vets_list.each do |vet|
-      @vets << VetSerializer.new(vet).serializable_hash[:data][:attributes]
+      @vets << {
+        data: VetSerializer.new(vet).serializable_hash[:data][:attributes],
+        specialization: SpecializationSerializer.new(vet.specialization).serializable_hash[:data][:attributes]
+      }
     end
     render json: @vets
   end
 
   def show
+    render json: {
+      data: VetSerializer.new(@vet).serializable_hash[:data][:attributes],
+      specialization: SpecializationSerializer.new(@vet.specialization).serializable_hash[:data][:attributes]
+    }
   end
 
   def destroy
   end
 
   def create
+  end
+
+  private
+
+  def set_vet
+    @vet = Vet.find(params[:id])
   end
 end
