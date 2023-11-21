705cb5d7ca57835055b3996fc588786ce3000dc1
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-21 12:49:57 +0100

    Serialize vet json response

--- app/serializers/vet_serializer.rb
+++ app/serializers/vet_serializer.rb
@@ -1,4 +1,10 @@
 class VetSerializer
   include JSONAPI::Serializer
-  attributes 
+  attributes :id, :name, :vet_photo, :available_from, :available_to, :fees, :bio, :specialization_id
+  attribute :updated_at do |vet|
+    vet.created_at && vet.created_at.strftime('%m/%d/%Y')
+  end
+  attribute :updated_at do |vet|
+    vet.updated_at && vet.updated_at.strftime('%m/%d/%Y')
+  end
 end
