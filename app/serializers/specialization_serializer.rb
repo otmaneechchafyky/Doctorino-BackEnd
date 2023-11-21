5a61fe37c6a2f5b76b89bfb5ab10a418a82f4f8d
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-21 13:19:51 +0100

    Update specialization serializer

--- app/serializers/specialization_serializer.rb
+++ app/serializers/specialization_serializer.rb
@@ -1,7 +1,4 @@
 class SpecializationSerializer
   include JSONAPI::Serializer
-  attributes :name
-  attribute :created_date do |specialization|
-    specialization.created_at && specialization.created_at.strftime('%m/%d/%Y')
-  end
+  attributes :id :name
 end
