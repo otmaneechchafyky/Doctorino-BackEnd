3176ede481ff62e82613c4cd720458aacfab3ccb
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-21 12:25:42 +0100

    Serialize animal json response

---
+++ app/serializers/animal_serializer.rb
@@ -0,0 +1,10 @@
+class AnimalSerializer
+  include JSONAPI::Serializer
+  attributes :id, :name, :animal_photo, :date_of_birth, :weight, :escape_attempts, :owner_id, :genre_id
+  attribute :created_at do |animal|
+    animal.created_at && animal.created_at.strftime('%m/%d/%Y')
+  end
+  attribute :updated_at do |animal|
+    animal.updated_at && animal.created_at.strftime('%m/%d/%Y')
+  end
+end
