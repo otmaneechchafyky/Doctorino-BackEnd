9027dd3b26fdd9237b491030438803c0f71fbb1f
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-21 12:54:52 +0100

    Add vets endpoint to retrive all vets

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -1,5 +1,11 @@
 class VetsController < ApplicationController
   def index
+    @vets = []
+    @vets_list = Vet.all
+    @vets_list.each do |vet|
+      @vets << VetSerializer.new(vet).serializable_hash[:data][:attributes]
+    end
+    render json: @vets
   end
 
   def show
