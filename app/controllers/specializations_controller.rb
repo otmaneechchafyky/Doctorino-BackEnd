dc69098561468e6eeeb12262c0184a4c100d4e9a
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-21 18:59:01 +0100

    Add index action to specializations controller

--- app/controllers/specializations_controller.rb
+++ app/controllers/specializations_controller.rb
@@ -1,4 +1,10 @@
 class SpecializationsController < ApplicationController
   def index
+    @specializations = []
+    @specializations_list = Specialization.all
+    @specializations_list.each do |specialization|
+      @specializations << SpecializationSerializer.new(specialization).serializable_hash[:data][:attributes]
+    end
+    render json: @specializations
   end
 end
