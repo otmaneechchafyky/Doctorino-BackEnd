1b33b5dee2cd74cde7466f4a3551765c428f32fa
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-21 19:29:41 +0100

    Add index action to appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,14 @@
 class AppointmentsController < ApplicationController
+    def index
+        @appointments = []
+        @appointments_list = Appointment.all
+        @appointments_list.each do |appointment|
+          @appointments << {
+            data: AppointmentSerializer.new(appointment).serializable_hash[:data][:attributes],
+            animal: AnimalSerializer.new(appointment.animal).serializable_hash[:data][:attributes],
+            vet: VetSerializer.new(appointment.vet).serializable_hash[:data][:attributes]
+          }
+        end
+        render json: @appointments
+    end
 end
