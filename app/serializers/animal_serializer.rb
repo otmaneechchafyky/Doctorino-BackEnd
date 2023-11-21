f47e776f1988bc06285643bc6606cbf1acb2ea85
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-21 19:12:31 +0100

    update animal serializer

--- app/serializers/animal_serializer.rb
+++ app/serializers/animal_serializer.rb
@@ -1,6 +1,6 @@
 class AnimalSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :animal_photo, :date_of_birth, :weight, :escape_attempts, :owner_id, :genre_id
+  attributes :id, :name, :animal_photo, :date_of_birth, :weight, :escape_attempts
   attribute :created_at do |animal|
     animal.created_at && animal.created_at.strftime('%m/%d/%Y')
   end
