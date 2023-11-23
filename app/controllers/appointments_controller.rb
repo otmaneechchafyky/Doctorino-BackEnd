54ae8364760989bcde2ef139b15d927f84aab1a5
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-23 15:07:43 +0100

    Add create action to appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -11,4 +11,22 @@ class AppointmentsController < ApplicationController
         end
         render json: @appointments
     end
+
+    def create
+        @appointment = Appointment.new(appointment_params)
+        if @appointment.save
+            render json: { message: 'Appointment created successfully.',
+            animal: AnimalSerializer.new(appointment.animal).serializable_hash[:data][:attributes],
+            vet: VetSerializer.new(appointment.vet).serializable_hash[:data][:attributes] },
+            status: :created
+        else
+            render json: { errors: @appointment.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def appointment_params
+        params.require(:appointment).permit(:date, :time, :location, :duration, :animal_id, :vet_id)
+    end
 end
